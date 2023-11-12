916044a0d91b5ae52fb3cbcc24d68af3a29a16ae
vmarin <vmarin.10@gmail.com>
2023-11-12 19:48:50 -0300

    Adicionado SwiperJS

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 5,
+    spaceBetween: 0,    
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
