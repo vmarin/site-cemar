7af6e99af8bf07d5adf3455e119ba8a929666cb4
vmarin <vmarin.10@gmail.com>
2023-11-16 09:42:25 -0300

    Página auto finalizada

--- js/script.js
+++ js/script.js
@@ -6,4 +6,8 @@ var swiper = new Swiper(".slide-content", {
       el: ".swiper-pagination",
       clickable: true,
     },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
   });
